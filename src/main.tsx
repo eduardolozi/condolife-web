1c982a4bca48643bbd9895f0015fb1260a32421f
eduardolozi <eduardoellozi2023@gmail.com>
2026-02-20 17:59:29 -0300

    add tanstack router and creates base architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { Provider } from './components/ui/provider.tsx'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen.ts'
+
+const router = createRouter({routeTree})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider forcedTheme='light'>
+      <RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
